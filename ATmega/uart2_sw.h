ababb76d665bb19d8e43c0405a736326a4dec2a9
dannyf00 <fdan00@live.com>
2018-03-02 21:03:45 -0500

    Add files via upload

---
+++ ATmega/uart2_sw.h
@@ -0,0 +1,35 @@
+#ifndef UART2_SW_H_INCLUDED
+#define UART2_SW_H_INCLUDED
+
+//software uart, timer0 for timebase, tx only
+//limitations:
+//1. TX only, polling
+//2. TIMER0 is used
+//3. MCU needs to run at at least 8Mhz: up to 38400bps tested to work on internal rc oscillator
+//4. Not all frequencies are equal / supported: F_CPU up to 8*256*UART baud rate
+//5. Be mindful of baud rate errors
+//@1Mhz: 2400bps confirmed to work
+//@8Mhz: 38400bps configrmed to work
+
+#include "gpio.h"
+
+//hardware configuration
+//end hardware configuration
+
+//global defines
+
+//global variables
+//look-up table for 4 bit reversal, in place
+//rest uart using usi + timer0 as clock
+//WGM02..0=0b010
+//1-start bit, 1-stop bit, 8-data
+void uart2_init(uint32_t bps);
+//send a char
+void uart2_put(char ch);
+
+//send a string
+void uart2_puts(char *str);
+
+char uart2_busy(void);
+
+#endif // UART0_USI_H_INCLUDED
