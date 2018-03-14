2ccb9446b2c2d9d4461704a33337f4d8ad9e4340
dannyf00 <fdan00@live.com>
2018-03-14 13:28:36 -0400

    Add files via upload

--- fifo.c
+++ fifo.c
@@ -6,8 +6,8 @@
 //global defines
 
 //initialize buffer
-FIFO_CNT fifo_init(FIFO_TypeDef *fifo, uint8_t *buffer, FIFO_CNT size) {
-	fifo->begin = 0;
+FIFOSIZE_T fifo_init(FIFO_TypeDef *fifo, uint8_t *buffer, FIFOSIZE_T size) {
+	fifo->start = 0;
 	fifo->end = 0;
 	fifo->buffer = buffer;
 	fifo->size = size;
@@ -15,13 +15,13 @@ FIFO_CNT fifo_init(FIFO_TypeDef *fifo, uint8_t *buffer, FIFO_CNT size) {
 }
 
 //reset fifo -> all data is lost and fifo becomes empty
-FIFO_CNT fifo_reset(FIFO_TypeDef *fifo) {
-	fifo->begin = fifo->end = 0;
+FIFOSIZE_T fifo_reset(FIFO_TypeDef *fifo) {
+	fifo->start = fifo->end = 0;
 	return fifo->size;
 }
 	
 //advance the end
-FIFO_CNT fifo_adv(FIFO_TypeDef *fifo, FIFO_CNT index) {
+FIFOSIZE_T fifo_adv(FIFO_TypeDef *fifo, FIFOSIZE_T index) {
 	if (++index == fifo->size) 	index = 0;
 	return index;
 }
@@ -35,17 +35,17 @@ uint8_t fifo_put(FIFO_TypeDef *fifo, uint8_t dat) {
 
 //get data from fifo
 uint8_t fifo_get(FIFO_TypeDef *fifo) {
-	uint8_t dat = fifo->buffer[fifo->begin];	//read the begining data point
-	fifo->begin = fifo_adv(fifo, fifo->begin);	//advance the beginning point
+	uint8_t dat = fifo->buffer[fifo->start];	//read the starting data point
+	fifo->start = fifo_adv(fifo, fifo->start);	//advance the startning point
 	return dat;
 }
 
 //if fifo is full
 uint8_t fifo_full(FIFO_TypeDef *fifo) {
-	return fifo_adv(fifo, fifo->end) == fifo->begin;
+	return fifo_adv(fifo, fifo->end) == fifo->start;
 }
 
 //if fifo is empty
 uint8_t fifo_empty(FIFO_TypeDef *fifo) {
-	return fifo->begin == fifo->end;
+	return fifo->start == fifo->end;
 }
